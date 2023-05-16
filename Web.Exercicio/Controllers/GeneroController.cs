ccdd23e77f0f17300ee1bd0aa6fbe3571f2eb37c
victor <victor.zaqui@uni9.edu.br>
2023-05-15 21:42:18 -0300

    first commit

---
+++ Web.Exercicio/Controllers/GeneroController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Exercicio.Models;
+using Web.Exercicio.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Exercicio.Controllers
+{
+    public class GeneroController : Controller
+    {
+        private IGeneroRepository _generoRepository;
+
+        public GeneroController(IGeneroRepository generoRepository)
+        {
+            _generoRepository = generoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Genero genero)
+        {
+            if (ModelState.IsValid)
+            {
+                _generoRepository.Cadastrar(genero);
+                _generoRepository.Salvar();
+                TempData["msg"] = "Cadastrado!";
+                return RedirectToAction("Cadastrar");
+            }
+            return View();
+        }
+    }
+}
---
+++ Web.Exercicio/Repositories/IJogoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Web.Exercicio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Web.Exercicio.Repositories
+{
+    public interface IJogoRepository
+    {
+        void Cadastrar(Jogo jogo);
+        void Atualizar(Jogo Jogo);
+        void Remover(int id);
+        IList<Jogo> Listar();
+        Jogo BuscarPorCodigo(int id);
+        IList<Jogo> BuscarPor(Expression<Func<Jogo, bool>> filtro);
+        void Salvar();
+
+        void Disponibilizar(int id);
+    }
+}
